c35e2d9c76bc48c60120b81a4a569315c03fb96f
Suvonov2000 <suvonov200@gmail.com>
2025-01-05 16:05:28 +0900

    small webs

--- src/clock/App_clock.jsx
+++ src/clock/App_clock.jsx
@@ -1,62 +1,65 @@
-import{useEffect, useState, useRef} from "react";
-import {Button} from "antd";
+import { Button } from "antd";
+import { useEffect, useRef, useState } from "react";
+
+
 
 
 function App () {
-    const timeRef = useRef(null);
+
     const [second, setSecond] = useState(0);
     const [minute, setMinute] = useState(0);
     const [hour, setHour] = useState(0);
     const [running, setRunning] = useState(false);
+    const timerRef = useRef(null);
 
-
-    const changeSecond = ((previous)=>{
-        if(previous === 59){
-            setMinute(changeMinute)
+    const changeSecond =(previous) =>{
+        if (previous === 59){
+            setMinute(changeMinute);
             return 0;
         }
-        return previous + 1
-    });
+        return previous +1
+    };
 
-    const changeMinute = ((previous)=>{
-        if(previous === 59){
+    const changeMinute =(previous) =>{
+        if (previous === 59){
             setHour((previous)=>{
-                return previous +1;
+                return previous +1
             })
             return 0;
         }
-        return previous + 1
-    });
+        return previous +1
+    };
 
 
 
-  useEffect (()=>{
-    if(running)
-        timeRef.current = setInterval(() => {
-            setSecond(changeSecond)
-        }, 10);
-    else clearInterval(timeRef.current);
-  },[running]);
+    useEffect(()=>{
+        if (running)
+        timerRef.current = setInterval(() => {
+            setSecond (changeSecond)
+        }, 1000);
+        else clearInterval(timerRef.current);
+    },[running]);
 
-     const reset = () =>{
+    const reset = () => {
         setSecond(0);
         setMinute(0);
         setHour(0);
-     };
+    };
 
-    return (
-        <div id="main" className="flex items-center justify-center">
-            <div className="w-[500px] h-[500px] bg-orange-500 mt-52">
+
+    return(
+
+        <div className="flex items-center justify-center">
+            <div className="w-[500px] h-[500px] bg-red-500 mt-52">
                 <div className="flex justify-center gap-[10px] text-7xl w-full mt-[20px]">
                     <h3>{hour}</h3>:<h3>{minute}</h3>:<h3>{second}</h3>
                 </div>
-                <div className="w-[80%] m-auto flex items-center justify-between mt-[20px]">
-                <Button type="primary">Tap</Button>
-                {running
-                ?<Button onClick={()=> setRunning(false)}>Pause</Button>
-                :<Button onClick={()=>setRunning(true)}>Start</Button>
-                }
-                <Button danger type="primary" onClick={reset}>REstart</Button>
+                <div className="w-[80%] m-auto flex justify-center gap-[50px] mt-[20px]">
+                  {running
+                   ?<Button onClick={()=>(setRunning(false))}>Pause</Button>
+                   :<Button onClick={()=> setRunning(true)}>Start</Button>
+                  }
+                   <Button onClick={reset}>Restart</Button>
                 </div>
             </div>
         </div>
