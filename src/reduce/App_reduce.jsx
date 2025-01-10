845a2d4f695302d05def738744345282ef60b2ba
Suvonov2000 <suvonov200@gmail.com>
2025-01-11 00:33:34 +0900

    react

--- src/reduce/App_reduce.jsx
+++ src/reduce/App_reduce.jsx
@@ -1,15 +1,26 @@
 import { Button } from "antd";
-import { useState } from "react"
+import { useState, useReducer } from "react"
 
-
+const reducer = (state, action) =>{
+    switch (action.type) {
+        case "increment":
+            return {count: state.count +1};
+        case "decrement":
+            return {count: state.count -1}
+            default:
+                console.error("You called dispatch without any type")
+    }
+    return {count: state.count +1}
+}
 
 const  App_reduce = () =>{
-    const [count, setCount] = useState(0);
+    // const [count, setCount] = useState(0);
+    const [state, dispatch] = useReducer(reducer, {count: 0});
     return (
         <div className="w-[100%] h-[100vh] flex justify-center items-center">
-            <Button>-</Button>
-            {count}
-            <Button>+</Button>
+            <Button primary onClick={()=>dispatch({type:"decrement"})}>-</Button>
+            {state.count}
+            <Button danger onClick={()=>dispatch({type: "increment"})}>+</Button>
         </div>
     );
 };
